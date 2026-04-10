c94f3457d6e3a65c3918a23d4d920b3d9494b6ad
José Alfredo Macias <jose.alfredo.macias@hotmail.com>
2026-04-10 15:51:44 -0600

    commit2

---
+++ Utils/Operations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Utils;
+
+class Operations{
+    public function add($num1,$num2){
+        return $num1 + $num2;
+    }
+}
+
+
+
+?>
--- namespace.php
+++ namespace.php
@@ -1,12 +1,12 @@
 <?php
 
-echo "NAMESPACE";
+require "Utils\Operations.php";
 
-echo "<br>";
-echo "<br>";
+use Utils\Operations;
 
+$op = new Operations();
 
-
+echo $op->add(2,3);
 
 
 ?>
