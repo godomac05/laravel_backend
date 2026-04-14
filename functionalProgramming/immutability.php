413bcf01db79488d344fd9ddba9aa9e028a62292
José Alfredo Macias <jose.alfredo.macias@hotmail.com>
2026-04-14 16:47:03 -0600

    commit140426

---
+++ functionalProgramming/immutability.php
@@ -0,0 +1,40 @@
+<?php
+
+//Inmutabilidad es la manera de protegernos a saber que ese objeto no se va a modificar
+
+class Location{
+    private float $x;
+    private float $y;
+
+    public function __construct(float $x, float $y){
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getX(): float {
+        return $this->x;
+    }
+
+    public function getY(): float {
+        return $this->y;
+    }
+
+    public function move(float $x, float $y): Location{
+        $location = new Location($this->x + $x, $this->y + $y);
+        return $location;
+    }
+
+}
+
+
+$location = new Location (1,2);
+//$location->x = 1.2;
+$newLocation = $location->move(10,22);
+
+echo $location->getX()." ".$location->getY()."<br>";
+echo $newLocation->getX()." ".$newLocation->getY()."<br>";
+
+
+
+
+ ?>
